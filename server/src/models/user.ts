5f2b2c8b91b9719b8eca36e01f742ce9e9019a59
Muma Dev <muma.sanmartin2011@gmail.com>
2022-09-07 19:59:08 -0300

    gitignore

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -2,10 +2,24 @@ import mongoose, { Schema } from "mongoose";
 import User from '../interfaces/user'
 
 const userSchema = new mongoose.Schema<User>({
-   _id : {required: false, type: Schema.Types.ObjectId},
-  username: String,
-  email: String,
-  password: {type: String, required: true},
+   _id : {
+    required: false, 
+    type: Schema.Types.ObjectId
+  },
+  username: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  email: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
   lastLogin: Date,
   role: String,
   active: Boolean,
